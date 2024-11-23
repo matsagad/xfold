a2e101358ea4444d677d9d2e8a6fe9dbbb989a98
matsagad <matsagad@gmail.com>
2024-11-23 20:13:07 +0800

    Implement recycling embedder

--- xfold/model/embedder.py
+++ xfold/model/embedder.py
@@ -19,24 +19,23 @@ class RelPos(nn.Module):
 
 class InputEmbedder(nn.Module):
     def __init__(
-        self, msa_embed_dim: int, pair_embed_dim: int, max_residue_distance: int = 32
+        self,
+        msa_embed_dim: int,
+        pair_embed_dim: int,
+        max_relative_residue_dist: int = 32,
     ) -> None:
         super().__init__()
-        residue_distance_bins = torch.arange(
-            -max_residue_distance, max_residue_distance + 1
-        ).float()
+        target_dim = AminoAcidVocab.vocab_size
+        self.linear_pair_embed_target1 = nn.Linear(target_dim, pair_embed_dim)
+        self.linear_pair_embed_target2 = nn.Linear(target_dim, pair_embed_dim)
 
-        self.linear_embed_target_to_pair1 = nn.Linear(
-            AminoAcidVocab.vocab_size, pair_embed_dim
-        )
-        self.linear_embed_target_to_pair2 = nn.Linear(
-            AminoAcidVocab.vocab_size, pair_embed_dim
-        )
-        self.relpos = RelPos(pair_embed_dim, residue_distance_bins)
-        self.linear_embed_msa_to_msa = nn.Linear(MSA.N_MSA_FEATS, msa_embed_dim)
-        self.linear_embed_target_to_msa = nn.Linear(
-            AminoAcidVocab.vocab_size, msa_embed_dim
-        )
+        residue_dist_bins = torch.arange(
+            -max_relative_residue_dist, max_relative_residue_dist + 1
+        ).float()
+        self.relpos = RelPos(pair_embed_dim, residue_dist_bins)
+
+        self.linear_msa_embed_msa = nn.Linear(MSA.N_MSA_FEATS, msa_embed_dim)
+        self.linear_msa_embed_target = nn.Linear(target_dim, msa_embed_dim)
 
     def forward(
         self,
@@ -44,13 +43,41 @@ class InputEmbedder(nn.Module):
         residue_index: torch.Tensor,
         msa_feat: torch.Tensor,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        a = self.linear_embed_target_to_pair1(target_feat)
-        b = self.linear_embed_target_to_pair2(target_feat)
+        a = self.linear_pair_embed_target1(target_feat)
+        b = self.linear_pair_embed_target2(target_feat)
         pair_rep = a.unsqueeze(1) + b.unsqueeze(0)
         pair_rep = pair_rep + self.relpos(residue_index)
 
-        msa_rep = self.linear_embed_msa_to_msa(
-            msa_feat
-        ) + self.linear_embed_target_to_msa(target_feat)
+        msa_rep = self.linear_msa_embed_msa(msa_feat) + self.linear_msa_embed_target(
+            target_feat
+        )
 
         return msa_rep, pair_rep
+
+
+class RecyclingEmbedder(nn.Module):
+    def __init__(
+        self, msa_embed_dim: int, pair_embed_dim: int, bins: torch.Tensor
+    ) -> None:
+        super().__init__()
+        n_bins = len(bins)
+        self.one_hot_cb_dist = OneHotNearestBin(bins)
+        self.linear_pair_embed_bins = nn.Linear(n_bins, pair_embed_dim)
+        self.layer_norm_pair = nn.LayerNorm((pair_embed_dim,))
+        self.layer_norm_msa = nn.LayerNorm((msa_embed_dim,))
+
+    def forward(
+        self,
+        target_msa_rep: torch.Tensor,
+        pair_rep: torch.Tensor,
+        prev_struct_rep_cb: torch.Tensor,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        # Embed CB atom distance information
+        cb_dist_matrix = torch.cdist(prev_struct_rep_cb, prev_struct_rep_cb, p=2)
+        pair_embed = self.linear_pair_embed_bins(self.one_hot_cb_dist(cb_dist_matrix))
+
+        # Embed Evoformer outputs
+        new_pair_rep = pair_embed + self.layer_norm_pair(pair_rep)
+        new_target_msa_rep = self.layer_norm_msa(target_msa_rep)
+
+        return new_target_msa_rep, new_pair_rep
--- xfold/model/model.py
+++ xfold/model/model.py
@@ -1,22 +1,29 @@
+import torch
 import torch.nn as nn
 from typing import List
-from xfold.model.embedder import InputEmbedder
+from xfold.model.embedder import InputEmbedder, RecyclingEmbedder
 from xfold.protein.sequence import MSA
-from xfold.protein.structure import ProteinFrames, ProteinStructure, TemplateProtein
+from xfold.protein.structure import ProteinStructure, TemplateProtein
 
 
 class AlphaFold2Config:
-    # Inference configs
+    # Inference
     n_recycling_iters: int = 3
     n_ensembles: int = 1
 
-    # MSA bootstrapping configs
+    # MSA bootstrapping
     max_n_msa_seqs: int = 512
     max_n_extra_msa_seqs: int = 1024
 
-    # Embedding configs
+    # Embedding
     msa_embed_dim: int = 256
     pair_embed_dim: int = 128
+    ## Input embedding
+    max_relative_residue_dist: int = 32
+    ## Recycling embedding
+    min_beta_carbon_dist: float = 3.375
+    max_beta_carbon_dist: float = 21.375
+    n_beta_carbon_dist_bins: int = 15
 
 
 class AlphaFold2(nn.Module):
@@ -28,6 +35,10 @@ class AlphaFold2(nn.Module):
         max_n_extra_msa_seqs: int = 1024,
         msa_embed_dim: int = 256,
         pair_embed_dim: int = 128,
+        max_relative_residue_dist: int = 32,
+        min_beta_carbon_dist: float = 3.375,
+        max_beta_carbon_dist: float = 21.375,
+        n_beta_carbon_dist: int = 15,
     ):
         super().__init__()
         self.n_cycling_iters = n_recycling_iters
@@ -35,7 +46,16 @@ class AlphaFold2(nn.Module):
         self.max_n_msa_seqs = max_n_msa_seqs
         self.max_n_extra_msa_seqs = max_n_extra_msa_seqs
 
-        self.input_embedder = InputEmbedder(msa_embed_dim, pair_embed_dim)
+        self.input_embedder = InputEmbedder(
+            msa_embed_dim, pair_embed_dim, max_relative_residue_dist
+        )
+
+        beta_carbon_bins = torch.linspace(
+            min_beta_carbon_dist, max_beta_carbon_dist, n_beta_carbon_dist
+        )
+        self.recycling_embedder = RecyclingEmbedder(
+            msa_embed_dim, pair_embed_dim, beta_carbon_bins
+        )
 
     def forward(
         self,
@@ -48,11 +68,11 @@ class AlphaFold2(nn.Module):
 
         residue_index = target.seq.seq_index
         target_feat = target.seq.seq_one_hot
-        n_res = target.seq.length()
+        N_res, N_coords = target.seq.length(), 3
 
         prev_avg_msa_rep = 0
         prev_avg_pair_rep = 0
-        prev_avg_struct_rep = ProteinFrames.zero_init(n_res)
+        prev_avg_struct_cb = torch.zeros((N_res, N_coords))
         for i_cycle in range(self.n_cycling_iters):
             avg_msa_rep = 0
             avg_pair_rep = 0
@@ -70,6 +90,12 @@ class AlphaFold2(nn.Module):
                 )
 
                 # Recycling embedder
+                target_msa_rep = msa_rep[0]
+                _target_msa_rep, _pair_rep = self.recycling_embedder(
+                    target_msa_rep, pair_rep, prev_avg_struct_cb
+                )
+                msa_rep[0] = target_msa_rep + _target_msa_rep
+                pair_rep = pair_rep + _pair_rep
 
                 # Templates embedder
 
