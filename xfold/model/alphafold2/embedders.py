Remove incorrect additional dimension in attention calculation

xfold/model/alphafold2/embedders.py
@@ -184,9 +184,9 @@ class TemplatePointwiseAttention(nn.Module):
         k, v = map(lambda u: u.view(temp_proj_shape), kv)
 
         a = F.softmax(
-            self.inv_sqrt_dim * torch.einsum("ijdh,sijdh->sijdh", q, k), dim=0
+            self.inv_sqrt_dim * torch.einsum("ijdh,sijdh->sijh", q, k), dim=0
         )
-        out = torch.einsum("sijdh,sijdh->ijdh", a, v)
+        out = torch.einsum("sijh,sijdh->ijdh", a, v)
         pair_update = self.out_proj(out.flatten(-2, -1))
 
         pair_rep = pair_rep + pair_update
@@ -263,9 +263,9 @@ class MSAColumnGlobalAttention(nn.Module):
         q = q.view(q_shape).mean(dim=0)
         g = self.to_g(msa_rep).view(q_shape)
 
-        a = F.softmax(self.inv_sqrt_dim * torch.einsum("idh,tid->tidh", q, k), dim=0)
+        a = F.softmax(self.inv_sqrt_dim * torch.einsum("idh,tid->tih", q, k), dim=0)
 
-        out = g * torch.einsum("tidh,tid->idh", a, v).unsqueeze(0)
+        out = g * torch.einsum("tih,tid->idh", a, v).unsqueeze(0)
         out = self.out_proj(out.flatten(-2, -1))
 
         return out

xfold/model/alphafold2/evoformer.py
@@ -59,14 +59,14 @@ class MSARowAttentionWithPairBias(nn.Module):
 
         qkv = self.to_qkv(msa_rep).chunk(3, dim=-1)
         q, k, v = map(lambda x: x.view(qkv_shape), qkv)
-        b = self.to_b(pair_rep).unsqueeze(-2).unsqueeze(0)
+        b = self.to_b(pair_rep).unsqueeze(0)
         g = self.to_g(msa_rep).view(qkv_shape)
 
         a = F.softmax(
-            self.inv_sqrt_dim * torch.einsum("sidh,sjdh->sijdh", q, k) + b, dim=2
+            self.inv_sqrt_dim * torch.einsum("sidh,sjdh->sijh", q, k) + b, dim=2
         )
 
-        out = g * torch.einsum("sijdh,sjdh->sidh", a, v)
+        out = g * torch.einsum("sijh,sjdh->sidh", a, v)
         out = self.out_proj(out.flatten(-2, -1))
 
         return out
@@ -93,10 +93,10 @@ class MSAColumnAttention(nn.Module):
         q, k, v = map(lambda x: x.view(qkv_shape), qkv)
         g = self.to_g(msa_rep).view(qkv_shape)
 
-        a = F.softmax(self.inv_sqrt_dim * torch.einsum("sidh,tidh->stidh", q, k), dim=1)
+        a = F.softmax(self.inv_sqrt_dim * torch.einsum("sidh,tidh->stih", q, k), dim=1)
 
         # Typo in paper? should be a^h_{sti} v^h_{ti} instead of a^h_{sti} v^h_{st}
-        out = g * torch.einsum("stidh,tidh->sidh", a, v)
+        out = g * torch.einsum("stih,tidh->sidh", a, v)
         out = self.out_proj(out.flatten(-2, -1))
 
         return out

xfold/model/common/triangle_ops.py
@@ -13,7 +13,7 @@ class TriangleAttentionBase(nn.Module):
 
         self.layer_norm = nn.LayerNorm(input_dim)
         self.to_qkv = LinearNoBias(input_dim, 3 * n_heads * proj_dim)
-        self.to_b = LinearNoBias(input_dim, n_heads * proj_dim)
+        self.to_b = LinearNoBias(input_dim, n_heads)
         self.to_g = LinearSigmoid(input_dim, n_heads * proj_dim)
         self.out_proj = nn.Linear(n_heads * proj_dim, input_dim)
 
@@ -28,14 +28,13 @@ class TriangleAttentionBase(nn.Module):
         raise NotImplementedError()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # x: [N_RES, N_RES, N_FEATS]
         qkv_shape = (*x.shape[:2], self.proj_dim, self.n_heads)
 
         # Project inputs
         x = self.layer_norm(x)
         qkv = self.to_qkv(x).chunk(3, dim=-1)
         q, k, v = map(lambda u: u.view(qkv_shape), qkv)
-        b = self.to_b(x).view(qkv_shape)
+        b = self.to_b(x)
         g = self.to_g(x).view(qkv_shape)
 
         # Compute attention
@@ -56,11 +55,11 @@ class TriangleAttentionStartingNode(TriangleAttentionBase):
     ) -> torch.Tensor:
         ## a_ijk := softmax_k( 1/sqrt(c) * q_ij^T k_ik + b_jk )
         a = F.softmax(
-            self.inv_sqrt_dim * torch.einsum("ijdh,ikdh->ijkdh", q, k) + b.unsqueeze(0),
+            self.inv_sqrt_dim * torch.einsum("ijdh,ikdh->ijkh", q, k) + b.unsqueeze(0),
             dim=2,
         )
         ## o_ij := g_ij \odot \sum_k a_ijk * v_ik
-        out = g * torch.einsum("ijkdh,ikdh->ijdh", a, v)
+        out = g * torch.einsum("ijkh,ikdh->ijdh", a, v)
         return out
 
 
@@ -75,12 +74,12 @@ class TriangleAttentionEndingNode(TriangleAttentionBase):
     ) -> torch.Tensor:
         ## a_ijk := softmax_k( 1/sqrt(c) * q_ij^T k_kj + b_ki )
         a = F.softmax(
-            self.inv_sqrt_dim * torch.einsum("ijdh,kjdh->ijkdh", q, k)
-            + torch.einsum("iokdh->koidh", b.unsqueeze(1)),
+            self.inv_sqrt_dim * torch.einsum("ijdh,kjdh->ijkh", q, k)
+            + torch.einsum("iokh->koih", b.unsqueeze(1)),
             dim=2,
         )
         ## o_ij := g_ij \odot \sum_k{ a_ijk * v_kj }
-        out = g * torch.einsum("ijkdh,kjdh->ijdh", a, v)
+        out = g * torch.einsum("ijkh,kjdh->ijdh", a, v)
         return out
 
 